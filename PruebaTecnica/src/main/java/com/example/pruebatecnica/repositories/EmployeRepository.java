16ba23e5211019da9cdd1b117e4b2130083c589b
GerardoHqz <gerardohenriquez.sv@gmail.com>
2024-06-26 21:19:55 -0600

    update bd

--- PruebaTecnica/src/main/java/com/example/pruebatecnica/repositories/EmployeRepository.java
+++ PruebaTecnica/src/main/java/com/example/pruebatecnica/repositories/EmployeRepository.java
@@ -2,10 +2,11 @@ package com.example.pruebatecnica.repositories;
 
 import com.example.pruebatecnica.models.entities.Employe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.UUID;
 
-public interface EmployeRepository extends JpaRepository<Employe, UUID> {
+public interface EmployeRepository extends JpaRepository<Employe, UUID>, QuerydslPredicateExecutor<Employe> {
     Employe findByCode(String code);
     void deleteByCode(String code);
 }
--- PruebaTecnica/src/main/java/com/example/pruebatecnica/services/implementations/EmployeServiceImpl.java
+++ PruebaTecnica/src/main/java/com/example/pruebatecnica/services/implementations/EmployeServiceImpl.java
@@ -3,9 +3,15 @@ package com.example.pruebatecnica.services.implementations;
 import com.example.pruebatecnica.models.dtos.EmployeDTO;
 import com.example.pruebatecnica.models.dtos.UpdateEmployeDTO;
 import com.example.pruebatecnica.models.entities.Employe;
+import com.example.pruebatecnica.models.entities.QEmploye;
 import com.example.pruebatecnica.repositories.EmployeRepository;
 import com.example.pruebatecnica.services.EmployeService;
 import com.example.pruebatecnica.utils.EmployeMapper;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
@@ -18,6 +24,16 @@ public class EmployeServiceImpl implements EmployeService {
 
     @Autowired
     EmployeRepository employeRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private JPAQueryFactory queryFactory;
+
+    @PostConstruct
+    public void init() {
+        this.queryFactory = new JPAQueryFactory(entityManager);
+    }
 
     @Override
     @Transactional(rollbackOn = Exception.class)
@@ -52,21 +68,32 @@ public class EmployeServiceImpl implements EmployeService {
 
     @Override
     public Employe getEmployeByCode(String code) {
-        return employeRepository.findByCode(code);
+        QEmploye qEmploye = QEmploye.employe;
+        BooleanExpression predicate = qEmploye.code.eq(code);
+        return queryFactory.selectFrom(qEmploye).where(predicate).fetchOne();
     }
 
     @Override
     @Transactional(rollbackOn = Exception.class)
     public void deleteEmployeByCode(String code) throws Exception{
-        try{
-            employeRepository.deleteByCode(code);
-        }catch(Exception e){
+        try {
+            QEmploye qEmploye = QEmploye.employe;
+            BooleanExpression predicate = qEmploye.code.eq(code);
+            Employe employe = queryFactory.selectFrom(qEmploye).where(predicate).fetchOne();
+
+            if (employe != null) {
+                employeRepository.delete(employe);
+            } else {
+                throw new NotFoundException();
+            }
+        } catch (Exception e) {
             throw new Exception("Cannot delete employe");
         }
     }
 
     @Override
     public List<Employe> getAllEmployes() {
-        return employeRepository.findAll();
+        QEmploye qEmploye = QEmploye.employe;
+        return queryFactory.selectFrom(qEmploye).fetch();
     }
 }
